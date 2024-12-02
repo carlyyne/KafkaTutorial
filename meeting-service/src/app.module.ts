d01030d3ba7c1eefcd30f75cb950c4f6df0a7aca
Carlyne BARRACHIN <113094666+carlyyne@users.noreply.github.com>
2024-12-02 19:00:20 +0200

    Update app.module.ts

--- meeting-service/src/app.module.ts
+++ meeting-service/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport, ClientKafka } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -11,18 +11,11 @@ import { AppService } from './app.service';
         transport: Transport.KAFKA,
         options: {
           client: {
-            clientId: 'nestjs-consumer-server',
-            brokers: ['localhost:29092', 'localhost:29093'], // Add all broker addresses
-            connectionTimeout: 10000, // Increased timeout
-            requestTimeout: 30000, // Increased timeout
-            retry: {
-              retries: 10, // Increased retries
-              initialRetryTime: 300,
-              maxRetryTime: 2000, // Increased max retry time
-            },
+            clientId: 'meeting',
+            brokers: ['localhost:29092'],
           },
           consumer: {
-            groupId: 'nestjs-group-server',
+            groupId: 'meeting-group',
           },
         },
       },
